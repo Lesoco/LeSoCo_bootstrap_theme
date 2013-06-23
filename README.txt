76bf792700404825d51d548e730726397e8ec540
lesoco <adrian.cowell@lesoco.ac.uk>
2013-06-23 22:36:04 +0100

    first commit

---
+++ README.txt
@@ -0,0 +1,6 @@
+This theme is based on the Bootstrap CSS framework, version 2.3
+
+
+RELESE NOTES VERSION 2013052300
+Revison to less files 
+
---
+++ lang/en/theme_bootstrap.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * Strings for component 'theme_bootstrap', language 'en', branch 'MOODLE_23_STABLE'
+ *
+ * @package   Bootstrap theme
+ * @copyright 2013
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['pluginname'] = 'Bootstrap';
+$string['region-side-post'] = 'Right';
+$string['region-side-pre'] = 'Left';
+$string['notes'] =  'Bootstrap Configuration page';
+$string['notesdesc'] = 'Configure your theme';
+$string['enableglyphicons'] = 'Enable Glyphicons';
+$string['enableglyphiconsdesc'] = 'Enable Glyphicons, use with Moodle 2.3 or older';
+$string['enablejquery'] = 'Enable jquery';
+$string['enablejquerydesc'] = 'Jquery is used in Bootstrap to enable the custom menu dropdown';
+$string['logo_url'] = 'Logo URL';
+$string['logo_urldesc'] = 'Enter the full URL of your custom logo, make sure it is of a proper size';
+$string['navlogo_url'] = 'Navigation Logo URL';
+$string['navlogo_urldesc'] = 'Enter the full URL of your navigation logo, it will be rendered inside the top menu';
+$string['navlogo_width'] = 'Navigation Logo width in px';
+$string['navlogo_widthdesc'] = 'Enter the width of the navlogo in pixels (default 40), this is used to make sure the theme menu renders okay';
+$string['navlogo_height'] = 'Navigation Logo height';
+$string['navlogo_heightdesc'] = 'Enter the height of your navigation logo in pixels (default 40), use a height of max 40px';
+$string['customcss'] = 'Custom CSS';
+$string['customcssdesc'] = 'Add your extra CSS in this box, use it to customize your theme with a little css';
+$string['shortennavbar'] = 'Shorten breadcrumb navbar';
+$string['shortennavbardesc'] = 'Enabling this setting shortens the navigation breadcrumb hiding the course category and submodule info';
+$string['showpurgecaches'] = 'Show purge caches option';
+$string['showpurgecachesdesc'] = 'Enable purging of caches link in navbar, this is only availble for admins and is used for Moodle development';
+
+$string['choosereadme'] = '
+<div class="clearfix"><div class="theme_screenshot"><h2>LeSoCo Bootstrap</h2>
+<img class=img-polaroid src="bootstrap/pix/screenshot.jpg" />
+<h3>Theme Credits</h3>
+<p>
+<h3>Lesoco them based on twitter Bootstrap for Moodle</h3>
+<p>
+This theme is based on the Bootstrap CSS framework, version 2.3
+</p>';
+
